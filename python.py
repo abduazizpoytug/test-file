31dcb14eb1b9a0a4b373853b801fceb2f0d4a145
abduaziz <104622399+abduazizpoytug@users.noreply.github.com>
2022-04-29 14:15:52 +0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,157 @@
+# file = open('index.html', 'r')
+# start = file.read()
+
+
+try:
+    file = open('file.txt', 'r')
+    start = file.read("jiijijijmkmkcxezzwzftgb")
+except:
+    file2 = open('index.html', 'r')
+    start2 = file2.read()
+    print(start2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# try:
+#     a = string
+#     b = 12
+#     result = a*b
+# except TypeError:
+#     print("bularni qoshib bolmaydi")
+
+
+
+# try:
+#     a = assa5
+#     b = "12"
+#     result = a*b
+# except NameError:
+#     print("hato sozlar")
+
+
+# try:
+#     a = "assa5"
+#     b = "aas"
+#     result = a-b
+# except TypeError:
+#     print("bular soz holos")
+
+
+# try:
+#     a = "assa5"
+#     b = 34
+#     result = a-b
+# except TypeError:
+#     print("hato yozdingiz")
+
+
+# try:
+#     a = string
+#     b = 34
+#     result = a/b
+# except TypeError:
+#     print("bu javob hatoku")
+
+
+# try:
+#     a = result
+#     b = result
+#     result = a-b
+# except TypeError:
+#     print("bu notogr")
+
+
+# try:
+#     a = {salom}
+#     b = 34
+#     result = a-b
+# except NameError:
+#     print("ism notogriku")
+    
+
+# try:
+#     name = a/123
+# except TypeError:
+#     print("harfni songa bolib bolmaydi")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# prog = input("ism yozing: ")
+# birinchi = input("ism yozing: ")
+# ikkinchi = input("ism yozing: ")
+# uchinchi = input("ism yozing: ")
+# torinchi = input("ism yozing: ")
+# beshinchi = input("ism yozing: ")
+# texts = sorted(prog, birinchi, ikkinchi, uchinchi, torinchi, beshinchi)
+# print(texts)
